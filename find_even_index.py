aee24bf356439714974aef041b2cd7375e8f26bb
ganapathi <ganapathiambore@gmail.com>
2019-07-01 12:52:32 +0530

    "Python Programms"

---
+++ find_even_index.py
@@ -0,0 +1,5 @@
+l=[0,1,2,3,4,5,6]
+even_index=l[0:7:2]
+odd_index=l[1:7:2]
+print(even_index)
+print(odd_index)
---
+++ sum_of_three_numbers.py
@@ -0,0 +1,9 @@
+def max_of_numbers(x,y,z):
+    if x>y and x>z:
+        print("x is greather",x)
+    elif y>x and y>z:
+        print("y is greather",y)
+    else:
+        print("z is greather",z)
+max_of_numbers(100000,1000,200)
+max_of_numbers(1,0,2)
