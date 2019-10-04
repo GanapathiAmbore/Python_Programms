700a27dd5821d0b43105f2218ef6dd363ef3a279
GanapathiAmbore <50941599+GanapathiAmbore@users.noreply.github.com>
2019-10-04 15:36:07 +0530

    Update downloading_files.py

--- downloading_files.py
+++ downloading_files.py
@@ -1,13 +1,7 @@
 #Python programm to download files from given urls
 from selenium import webdriver
-driver=webdriver.Chrome('/home/ganapathi/Downloads/chromedriver')
-links=[
-    'https://login.globaldata.com/Login/IPLogin?ReferrerIC=ph&ReturnUrl=/Reports/ExportFullReportToWord/Respiratory-Syncytial-Virus--Epidemiology-Forecast-in-Asia-Pacific-Markets-to-2028',
-    'https://login.globaldata.com/Login/IPLogin?ReferrerIC=ph&ReturnUrl=/Reports/ExportFullReportToWord/Female-Infertility--Opportunity-Analysis-and-Forecasts-to-2028',
-    'https://login.globaldata.com/Login/IPLogin?ReferrerIC=ph&ReturnUrl=/Reports/ExportFullReportToWord/Myasthenia-Gravis--Epidemiology-Forecast-to-2028',
-    'https://login.globaldata.com/Login/IPLogin?ReferrerIC=ph&ReturnUrl=/Reports/ExportFullReportToWord/Ovarian-Cancer--Opportunity-Analysis-and-Forecasts-to-2028',
-    'https://login.globaldata.com/Login/IPLogin?ReferrerIC=ph&ReturnUrl=/Reports/ExportFullReportToWord/Gene-Therapy-in-Neurology'
-]
+driver=webdriver.Chrome(path of driver)
+links=["Define links here"]
 for link in links:
     print(link)
     print('Downloaded Link:',link)
